9ad890136d435361a522b0ffbd91e241fa546571
Emilio Wuerges <wuerges@gmail.com>
2019-10-29 17:17:52 -0300

    fixed declarator.hpp

--- test/generators.hpp
+++ test/generators.hpp
@@ -10,30 +10,27 @@ using namespace geometry;
 namespace rc {
 
 
+template <int NDIM>
+const auto genPT = gen::container<array<int, NDIM>>(gen::inRange(0, 100000));
 
-  const auto genPT = gen::container<array<int, 3>>(gen::inRange(0, 100000));
-
-  template<int NDIM>
-  struct Arbitrary<PT<NDIM>> {
-    static Gen<PT> arbitrary() {
-      return gen::map(genPT,
-        [](array<int, 3> vs) {
-          return PT(vs[0], vs[1], vs[2]);
+template<int NDIM>
+struct Arbitrary<PT<NDIM>> {
+    static Gen<PT<NDIM>> arbitrary() {
+    return gen::map(genPT<NDIM>,
+        [](array<int, NDIM> vs) {
+            return PT<NDIM>(vs);
         });
     }
-  };
+};
 
-  template<int NDIM>
-  struct Arbitrary<Shape<NDIM>> {
-    static Gen<Shape> arbitrary() {
-      return gen::map(gen::container<array<PT, 2>>(gen::arbitrary<PT>()),
-        [](const array<PT, 2> pts) {
-            PT a = min(pts[0], pts[1]);
-            PT b = max(pts[0], pts[1]);
-            // a.z = b.z;
-          return Shape(a, b);
+template<int NDIM>
+struct Arbitrary<Shape<NDIM> > {
+    static Gen<Shape<NDIM> > arbitrary() {
+    return gen::map(gen::container<array<PT<3>, 2> >(gen::arbitrary<PT<3>>()),
+        [](const array<PT<3>, 2> pts) {
+            return Shape<NDIM>(pts[0], pts[1]);
         });
     }
-  };
+};
 
 }
