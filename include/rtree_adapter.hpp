0d2cf31847b76291a15a7a0fd769e2f027327291
Emilio Wuerges <wuerges@gmail.com>
2019-10-29 17:08:18 -0300

    changed rtree adapter.hpp

--- include/rtree_adapter.hpp
+++ include/rtree_adapter.hpp
@@ -4,44 +4,140 @@
 #include <RTree.h>
 
 #include <vector>
-
+#include <set>
 
 namespace geometry {
 
 using std::vector;
 
-
-
+template <int NDIM>
 class RTree {
 
-    vector<Shape> shapes;
+    using Shapei = Shape<NDIM>;
+    using PTi = PT<NDIM>;
+
+    vector<Shapei> shapes;
 
 public:
-    using MyTree = rtree::RTree<int, int, 3, double>;
+    using MyTree = rtree::RTree<int, int, NDIM, double>;
     MyTree tree;
-    void populate(const std::vector<Shape> &param);
-    void add(const Shape &shape);
+    
+    void populate(const std::vector<Shapei> &param) {
+        for (const Shapei &s : param) {
+            add(s);
+        }
+    }
 
-    bool hits(const PT l, const PT r) const;
-    std::vector<Shape> collect(const PT l, const PT r) const;
-    std::vector<Shape> neighboors_diamond(const Shape &u, size_t number) const;
-    std::vector<Shape> collect_diamond(const Shape & center, unsigned radius) const;
-    std::vector<Shape> collect_diamond_2(const Shape & center, unsigned radius1, unsigned radius2) const;
+    void add(const Shapei &shape) {
+        int id = shapes.size();
+        shapes.push_back(shape);
+        
+        int v1[3], v2[3];
+        to_intv(shape.a, v1);
+        to_intv(shape.b, v2);
+        
+        tree.Insert(v1, v2, id);
+    }
+
+    // bool hits(const PTi l, const PTi r) const;
+    // std::vector<Shapei> collect(const PTi l, const PTi r) const;
+    // std::vector<Shapei> neighboors_diamond(const Shapei &u, size_t number) const;
+    // std::vector<Shapei> collect_diamond(const Shapei & center, unsigned radius) const;
+    // std::vector<Shapei> collect_diamond_2(const Shapei & center, unsigned radius1, unsigned radius2) const;
   
 
-    int visit(const Shape & center, 
-        const std::function <bool (const Shape &)>& f) const ;
+    int visit(const Shapei & center, 
+        const std::function <bool (const Shapei &)>& f) const {
+        auto p = to_rect(center);    
+        return tree.Search(p.m_min, p.m_max, [&f,this](int i) { return f(shapes[i]); });
+    }
 
-    int visit_diamond(const Shape & center, unsigned radius, 
-        const std::function <bool (const Shape &)>& f) const;
-    int visit_diamond_2(const Shape & center, unsigned radius1, unsigned radius2, 
-        const std::function <bool (const Shape &)>& f) const;
-    
-    
+    int visit_diamond(const Shapei & center, unsigned radius, 
+        const std::function <bool (const Shapei &)>& f) const {
+        auto p = to_rect(center);    
+        return tree.SearchDiamond(p.m_min, p.m_max, radius, [&f,this](int i)->bool { return f(shapes[i]); });
+    }
+
+    int visit_diamond_2(const Shapei & center, unsigned radius1, unsigned radius2, 
+        const std::function <bool (const Shapei &)>& f) const {
+        auto p = to_rect(center);
+        return tree.SearchDiamond(p.m_min, p.m_max, radius1,  radius2, [&f,this](int i)->bool { return f(shapes[i]); });
+    }
+  
+
+    bool hits(const PTi l, const PTi r) const {
+        bool found = false;
+        const Shapei center{l, r};
+        this->visit(center, [&found] (const Shapei & x) ->bool {
+            found = true;
+            return false;
+        });
+        return found;    
+    }
+    std::vector<Shapei> collect(const PTi l, const PTi r) const {
+        std::vector<Shapei> res;
+        std::set<Shapei> temp;
+        
+        const Shapei center{l,r};
+        visit(center, [&temp](const Shapei & x) {
+            temp.insert(x);
+            return true;
+        });
+        for(auto & x : temp) { res.push_back(x); }
+        return res;
+    }
+
+    std::vector<Shapei> neighboors_diamond(const Shapei &u, size_t number) const {
+
+        std::vector<Shapei> res;
+
+        int a = 0, b = 1;
+        const int MAXB = 1e8;
+        while (res.size() < number && b < MAXB) {
+            visit_diamond_2(u, a, b, [&res](const Shapei & v) {
+                // if(collides(u, v)) {
+                res.push_back(v);
+                // }
+                return true;
+            });
+            a = b;
+            b = b*2;
+        }
+        return res;
+    }
+
+
+    std::vector<Shapei> collect_diamond(const Shapei & u, unsigned radius) const {
+        std::vector<Shapei> res;
+        visit_diamond(u, radius, [&res](const Shapei & v) {
+            res.push_back(v);
+            return true;
+        });
+        return res; 
+
+    }
+    std::vector<Shapei> collect_diamond_2(const Shapei & u, unsigned radius1, unsigned radius2) const {
+    std::vector<Shapei> res;
+        visit_diamond_2(u, radius1, radius2, [&res](const Shapei & v) {
+            res.push_back(v);
+            return true;
+        });
+        return res; 
+    }
 };
 
-void to_intv(const PT & p, int* v);
-RTree::MyTree::Rect to_rect(const Shape & s);
+template <int NDIM>
+void to_intv(const PT<NDIM> & p, int* v) {
+    return p.coord;
+}
+
+// RTree::MyTree::Rect to_rect(const Shape & s) {
+//     RTree::MyTree::Rect r;
+//     to_intv(s.a, r.m_min);    
+//     to_intv(s.b, r.m_max);
+//     return r;
+// }
+
 
 }
 
